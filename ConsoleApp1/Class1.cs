bc3d572dff0228fc601eb6995a79087a11b37402
Aleksandr <sanya.romaniuta@gmail.com>
2023-08-14 12:23:46 +0300

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,61 @@
+﻿using Renci.SshNet;
+using System.Net;
+using Throw;
+
+namespace ConsoleApp1;
+
+public class Class1
+{
+    public string Chain { get; private set; }
+    private SshClient Client { get; set; }
+    public Class1(string chain, SshClient client)
+    {
+        Client = client;
+        chain.Throw(s => throw new ArgumentException("Chain parameter is NullOrEmptyOrWhiteSpace.")).IfNullOrWhiteSpace(_ => _);
+        Chain = chain;
+    }
+    public string Ping(string hostname)
+    {
+        Client.Connect();
+        var cmd = Client.CreateCommand($"ping -c 5 {hostname} -q");
+        var res = cmd.Execute();
+        Client.Disconnect();
+        return res;
+    }
+
+    public void AddForwardingRule(ushort sourcePort, string localIP, ushort localPort, ForwardProtocol proto)
+    {
+
+        Client.Connect();
+        SshCommand addRuleCommand = null!;
+        switch (proto)
+        {
+            case ForwardProtocol.TCP:
+                addRuleCommand = Client.CreateCommand($"iptables -t nat -A {Chain} -p tcp --dport {sourcePort} -j DNAT --to-destination {localIP}:{localPort}");
+                break;
+            case ForwardProtocol.UDP:
+                addRuleCommand = Client.CreateCommand($"iptables -t nat -A {Chain} -p udp --dport {sourcePort} -j DNAT --to-destination {localIP}:{localPort}");
+                break;
+            case ForwardProtocol.Both:
+                addRuleCommand = Client.CreateCommand($"iptables -t nat -A {Chain} -p tcp --dport {sourcePort} -j DNAT --to-destination {localIP}:{localPort} && " +
+                    $"iptables -t nat -A {Chain} -p udp --dport {sourcePort} -j DNAT --to-destination {localIP}:{localPort}");
+                break;
+        }
+        addRuleCommand.Execute();
+        Client.Disconnect();
+
+        PrintRules();
+    }
+    public void PrintRules() 
+    {
+        Client.Connect();
+
+        var printRulesCommand = Client.CreateCommand($"iptables  -L {Chain} -t nat --line-numbers -n");
+        string rules = printRulesCommand.Execute();
+
+        Client.Disconnect();
+        Console.WriteLine(rules);
+    }
+}
+
+public enum ForwardProtocol { TCP, UDP, Both }
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp1;
+using Microsoft.Extensions.Configuration;
+using Renci.SshNet;
+using System.Net.NetworkInformation;
+
+
+var conf = new ConfigurationBuilder().AddJsonFile(@"C:\Users\sanchous\Desktop\epf.json").Build();
+string hostname = conf.GetSection("hostname").Value!;
+string username = conf.GetSection("username").Value!;
+string password = conf.GetSection("password").Value!;
+string chain = "vserver";
+
+
+
+
+using (var client = new SshClient(hostname, username, password))
+{
+    /*Class1.AddForwardingRule(client, 12345, "192.168.1.181", 54321, ForwardProtocol.Both);*/
+    var p = new Class1(chain, client);
+    p.PrintRules();
+}
